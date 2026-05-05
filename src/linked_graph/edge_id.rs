24906b230298fb31c0ffd9fb22d6663e3f788767
John Williams <jrw@pobox.com>
2026-05-05 12:51:43 -0700

    Restored LinkedGraph

---
+++ src/linked_graph/edge_id.rs
@@ -0,0 +1,106 @@
+use std::{
+    fmt::Debug,
+    hash::Hash,
+    marker::PhantomData,
+    sync::{Arc, Weak},
+};
+
+use derivative::Derivative;
+
+use crate::{EdgeIdTrait, Graph, directedness::DirectednessTrait, graph_id::GraphIdClone};
+
+use super::{Edge, NodeId};
+
+/// Edge identifier for [`LinkedGraph`](super::LinkedGraph).
+///
+/// Contains a weak pointer to the edge data and a graph ID for safety checks.
+#[derive(Derivative)]
+#[derivative(Clone(bound = "G::Directedness: Clone"))]
+pub struct EdgeId<G: Graph> {
+    pub(super) ptr: Weak<Edge<G>>,
+    pub(super) graph_id: GraphIdClone,
+    pub(super) directedness: G::Directedness,
+}
+
+// SAFETY: EdgeId is Send and Sync because it only contains a Weak pointer and
+// PhantomData, and does not allow mutation of the underlying data. The EdgeId
+// can only be used to access the edge data through Graph methods that ensure
+// the graph is still valid, so it cannot be used after the graph has been
+// dropped.
+unsafe impl<G: Graph> Send for EdgeId<G> {}
+unsafe impl<G: Graph> Sync for EdgeId<G> {}
+
+impl<G: Graph> Debug for EdgeId<G> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "EdgeId({:?}, {:?})", self.ptr, self.graph_id)
+    }
+}
+
+impl<G: Graph> PartialEq for EdgeId<G> {
+    fn eq(&self, other: &Self) -> bool {
+        self.ptr.as_ptr() == other.ptr.as_ptr()
+    }
+}
+
+impl<G: Graph> Eq for EdgeId<G> {}
+
+impl<G: Graph> Hash for EdgeId<G> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        (self.ptr.as_ptr() as usize).hash(state);
+    }
+}
+
+impl<G: Graph> PartialOrd for EdgeId<G> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<G: Graph> Ord for EdgeId<G> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.ptr.as_ptr().cmp(&other.ptr.as_ptr())
+    }
+}
+
+impl<G: Graph> EdgeIdTrait for EdgeId<G> {
+    type NodeId = NodeId<G>;
+    type Directedness = G::Directedness;
+
+    fn directedness(&self) -> Self::Directedness {
+        self.directedness
+    }
+
+    fn left(&self) -> NodeId<G> {
+        self.ptr
+            .upgrade()
+            .map(|edge| NodeId {
+                ptr: Arc::downgrade(
+                    &edge
+                        .ends
+                        .first()
+                        .ptr
+                        .upgrade()
+                        .expect("Source node dangling"),
+                ),
+                graph_id: self.graph_id,
+            })
+            .expect("EdgeId is dangling")
+    }
+
+    fn right(&self) -> NodeId<G> {
+        self.ptr
+            .upgrade()
+            .map(|edge| NodeId {
+                ptr: Arc::downgrade(
+                    &edge
+                        .ends
+                        .second()
+                        .ptr
+                        .upgrade()
+                        .expect("Target node dangling"),
+                ),
+                graph_id: self.graph_id,
+            })
+            .expect("EdgeId is dangling")
+    }
+}
---
+++ src/linked_graph/node_id.rs
@@ -0,0 +1,61 @@
+use std::{fmt::Debug, hash::Hash, sync::Weak};
+
+use crate::{Graph, graph_id::GraphIdClone};
+
+use super::Node;
+
+/// Node identifier for [`LinkedGraph`](super::LinkedGraph).
+///
+/// Contains a weak pointer to the node data and a graph ID for safety checks.
+pub struct NodeId<G: Graph> {
+    pub(super) ptr: Weak<Node<G>>,
+    pub(super) graph_id: GraphIdClone,
+}
+
+// SAFETY: See comment on EdgeId.
+unsafe impl<G: Graph> Send for NodeId<G> {}
+unsafe impl<G: Graph> Sync for NodeId<G> {}
+
+impl<G: Graph> Debug for NodeId<G> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NodeId({:?})", self.ptr.as_ptr())
+    }
+}
+
+impl<G: Graph> Clone for NodeId<G> {
+    fn clone(&self) -> Self {
+        NodeId {
+            ptr: Weak::clone(&self.ptr),
+            graph_id: self.graph_id,
+        }
+    }
+}
+
+impl<G: Graph> PartialEq for NodeId<G> {
+    fn eq(&self, other: &Self) -> bool {
+        self.ptr.as_ptr() == other.ptr.as_ptr() && self.graph_id == other.graph_id
+    }
+}
+
+impl<G: Graph> Eq for NodeId<G> {}
+
+impl<G: Graph> Hash for NodeId<G> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.ptr.as_ptr().hash(state);
+        self.graph_id.hash(state);
+    }
+}
+
+impl<G: Graph> PartialOrd for NodeId<G> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<G: Graph> Ord for NodeId<G> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.ptr.as_ptr().cmp(&other.ptr.as_ptr())
+    }
+}
+
+impl<G: Graph> crate::graph_traits::NodeIdTrait for NodeId<G> {}
